e52be56b2a77690d469a64d9202b48f67dceb5c7
Michael Bonner <michaelwbonner@gmail.com>
2020-04-17 12:31:27 -0600

    initial commit

---
+++ database/factories/TaskListFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\TaskList;
+use Faker\Generator as Faker;
+
+$factory->define(TaskList::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/migrations/2020_04_17_173505_create_tasks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('task_list_id');
+            $table->unsignedBigInteger('teamwork_id');
+            $table->string('name')->nullable();
+            $table->json('data')->nullable();
+            $table->timestamps();
+
+            $table->foreign('task_list_id')
+                ->references('id')->on('task_lists')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
